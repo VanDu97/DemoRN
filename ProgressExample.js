f13caa430b96d7dd703ef5edddcbc6804dda6cfc
VanDu <35896548+VanDu97@users.noreply.github.com>
2020-06-07 20:23:32 +0700

    update

--- ProgressExample.js
+++ ProgressExample.js
@@ -1,31 +1,47 @@
 import React, { Component } from "react";
 import * as Progress from "react-native-progress";
-import { View, Text, Dimensions } from "react-native";
+import { View, Text, Dimensions, TouchableOpacity } from "react-native";
 import { Surface, Shape } from "@react-native-community/art";
+var time;
 export default class ProgressExample extends Component {
   constructor(props) {
     super(props);
-    this.offset = 0;
-    this.time;
+    this.state = {
+      offset: 0,
+    };
   }
   componentDidMount() {
-    this.time = setTimeout(() => {
-      this.offset = this.offset + 0.5;
-    }, 1000);
+    this.handleTime();
   }
   componentWillUnmount() {
-    //this.time.clearTimeout();
+    clearInterval(time);
   }
+
+  handleTime = () => {
+    time = setInterval(() => {
+      //alert(1);
+
+      this.setState({ offset: this.state.offset + 0.1 });
+    }, 1000);
+  };
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Hello</Text>
+        <TouchableOpacity
+          onPress={() => {
+            this.setState({
+              offset: this.state.offset + 0.1,
+            });
+          }}
+        >
+          <Text>Next</Text>
+        </TouchableOpacity>
         <Progress.Bar
-          progress={this.offset}
+          progress={this.state.offset}
           width={Dimensions.get("window").width * 0.95}
           height={25}
           borderRadius={4}
-          animationType={"decay"}
         />
         <Progress.Pie progress={0.6} width={300} />
         <Progress.Circle progress={0.5} width={300} />
